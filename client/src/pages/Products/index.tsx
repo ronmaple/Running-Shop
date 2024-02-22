24ce58a1f2747d1ea3a2c4aa1babfad145e5c42e
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2024-02-21 20:43:34 -0800

    products-page: basic product list page

--- client/src/pages/Products/index.tsx
+++ client/src/pages/Products/index.tsx
@@ -1,7 +1,106 @@
-const Products = () => {
+import { useEffect, useState } from 'react'
+import { shoes } from '../../../../server/seeders/products'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from '@mui/material'
+
+export type Product = {
+  id: string
+  title: string
+  description?: string
+  unitPrice: number
+  salePrice: number
+  inventory?: number
+  images: string[]
+}
+interface ResponseData {
+  data: any[]
+  total: number
+  cursor: any
+}
+
+interface ProductResponse extends ResponseData {
+  data: Product[]
+  total: number
+  cursor: any
+}
+
+const getProducts = async () => {
+  const response = await fetch('http://localhost:3000/products')
+  const data = await response.json()
+  // todo: { standardize server response }
+  return data
+}
+
+// TODO:
+// 1. environment variable for url
+const Products = (props) => {
+  const [products, setProducts] = useState<ProductResponse>({
+    data: [],
+    total: 0,
+    cursor: null,
+  })
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<Error>()
+
+  useEffect(() => {
+    // todo make this better
+    setLoading(true)
+    getProducts()
+      .then((res) => {
+        console.log(res)
+        setProducts(res)
+      })
+      .catch((err) => {
+        setError(err)
+      })
+    setLoading(false)
+  }, [])
+
   return (
     <div>
-      <h1>Products</h1>
+      <>
+        {loading ? (
+          <div>loading...</div>
+        ) : (
+          <Container sx={{ py: 8 }} maxWidth="md">
+            <Grid container spacing={4}>
+              {products.data.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{
+                      height: '100%',
+                      display: 'flex',
+                      flexDirection: 'column',
+                    }}
+                  >
+                    <CardMedia
+                      component="div"
+                      sx={{
+                        // 4:3
+                        pt: '75%',
+                      }}
+                      image={`http://localhost:3000/static/${product.images[0]}`}
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {product.title}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        )}
+      </>
     </div>
   )
 }
