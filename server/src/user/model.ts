702048d617507fb7ee665afc98f85dd6db454a0e
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2024-02-25 15:54:36 -0800

    55: mongoose schema transformation & updated tests

--- server/src/user/model.ts
+++ server/src/user/model.ts
@@ -15,34 +15,47 @@ export type User = {
   role: Roles
 }
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      unique: false,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      unique: false,
+      required: true,
+    },
+    password: {
+      type: String,
+      minlength: 12,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: Roles.Basic,
+      enum: Roles,
+      required: true,
+    },
   },
-  firstName: {
-    type: String,
-    unique: false,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    unique: false,
-    required: true,
-  },
-  password: {
-    type: String,
-    minlength: 12,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: Roles.Basic,
-    enum: Roles,
-    required: true,
-  },
-})
+  {
+    // todo: standardize time formats
+    timestamps: true,
+    toJSON: {
+      transform: function (doc, ret) {
+        ret.id = ret._id.toString()
+        delete ret._id
+        delete ret.__v
+      },
+    },
+  }
+)
 
 const user = mongoose.model('user', userSchema)
 
